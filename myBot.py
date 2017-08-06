201cbe8a0410fdffa1f1de15e5141245bbea56ff
hallelbb <hallel.segel@gmail.com>
2017-08-06 16:01:41 +0300

    start choosing dest

--- myBot.py
+++ myBot.py
@@ -7,15 +7,16 @@ def do_turn(pw):
     if len(pw.my_planets()) == 0: #don't act if none of the planets are ours
         return
 
+    source = pw.my_planets()[0]#send from one of our planets
+
     if len(pw.neutral_planets()) >= 1: #send to the first of the neutral planets if there are any
         dest = pw.neutral_planets()[0]
         for i in pw.neutral_planets():
-            if ()
+            if (calculateDistance(i, source) < calculateDistance(dest,source)):
+                dest = i
     else:
         if len(pw.enemy_planets()) >= 1: #otherwise send to one of the opponent's planets
             dest = pw.enemy_planets()[0]
-
-    source = pw.my_planets()[0]#send from one of our planets
 
     num_ships = dest.num_ships() #send half of our ships
     pw.debug('Num Ships: ' + str(num_ships))
@@ -28,4 +29,4 @@ def calculateDistance(dst, src):
     yDst = dst.y()
     xSrc = src.x()
     ySrc = src.y()
-    return math.sqrt((xDst-xSrc)^2 + (yDst-ySrc)^2)
+    return math.sqrt(math.pow(xDst-xSrc, 2) + math.pow(yDst-ySrc, 2))
