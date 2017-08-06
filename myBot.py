2b66255c25e9d26364007a30710442a4785049d2
hallelbb <hallel.segel@gmail.com>
2017-08-06 17:21:37 +0300

    choose src based on highest num_ships

--- myBot.py
+++ myBot.py
@@ -4,22 +4,22 @@ def do_turn(pw):
     if len(pw.my_planets()) == 0: #don't act if none of the planets are ours
         return
 
-    source = pw.my_planets()[0] #send from one of our planets
-    for i in pw.my_planets():
-            if (i.num_ships() > source.num_ships()):
-                source = i
-
-    if len(pw.neutral_planets()) >= 1: #send to the first of the neutral planets if there are any
-        dest = pw.neutral_planets()[0]
-        for i in pw.neutral_planets():
-            if (pw.distance(source, i) < pw.distance(source, dest)):
-                dest = i
-    else:
-        if len(pw.enemy_planets()) >= 1: #otherwise send to one of the opponent's planets
-            dest = pw.enemy_planets()[0]
+    for source in pw.my_planets():
+        if len(pw.not_my_planets()) >= 1: #otherwise send to one of the opponent's planets
+            dest = pw.not_my_planets()[0]
+            for i in pw.not_my_planets():
+                if (pw.distance(source, i)*i.num_ships() < pw.distance(source, dest)*dest.num_ships()):
+                    dest = i
+        else:
+            dest = pw.my_planets()[0]
 
 
-    num_ships = source.num_ships()
-    pw.debug('Num Ships: ' + str(num_ships))
+        num_ships = min(source.num_ships()/2, dest.num_ships())
+        pw.debug('Num Ships: ' + str(num_ships))
 
-    pw.issue_order(source, dest, num_ships) #execute command to send fleet
+        pw.issue_order(source, dest, num_ships) #execute command to send fleet
+#fleetExists = 0
+            #for j in pw.fleets():
+            #    if (j.destination_planet() == dest and owner == 1):
+            #        fleetExists = 1
+            #if fleetExists==0:
